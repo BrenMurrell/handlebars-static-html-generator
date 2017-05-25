d11ffb31fae2b51e0889ca0aeb4f14ee697e38bc
BrenMurrell <bren.murrell@nvinteractive.com>
2017-05-25 12:49:47 +1200

    adding automation to default task

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,28 @@ var gulp = require('gulp');
 var handlebars = require('gulp-compile-handlebars');
 var rename = require('gulp-rename');
 var filelist = require('gulp-filelist');
-var templateList = require('./filelist.json');
+var fs = require('fs');
 
 gulp.task("createFileIndex", function(){
 		gulp.src(['./src/*.*'])
       .pipe(filelist('filelist.json', { flatten: true, removeExtensions: true }))
       .pipe(gulp.dest("./"));
 });
-gulp.task('default', function () {
+
+gulp.task("default", function() {
+	gulp.watch('src/*.handlebars',{cwd:'./'}, ['createFileIndex'])
+	gulp.watch('src/**/*.handlebars',{cwd:'./'}, ['compile'])
+	gulp.watch('filelist.json',{cwd:'./'},['compile']);
+});
+
+gulp.task('compile', function () {
+	var templateList = JSON.parse(fs.readFileSync("./filelist.json", "utf8"));
 	var templateData = {
 		firstName: 'Bren',
 		templates: templateList
 	},
 	options = {
 		ignorePartials: true, //ignores the unknown footer2 partial in the handlebars template, defaults to false
-		// partials : {
-		// 	footer : '<footer>the end</footer>'
-		// },
 		batch : ['./src/partials'],
 		helpers : {
 			capitals : function(str){
@@ -26,9 +31,6 @@ gulp.task('default', function () {
 			}
 		}
 	}
-	//templates = ['hello', 'goodbye', 'test'];
-	//gulp.start('goodbye');
-
 	var doAllTemplates = function() {
 		for (var i = 0; i <  templateList.length; i++) {
 			compileTemplate(templateList[i]);
@@ -40,7 +42,5 @@ gulp.task('default', function () {
 			.pipe(rename(templateName + '.html'))
 			.pipe(gulp.dest('dist'));
 	}
-
 	doAllTemplates();
-
 });
